ee733e10dcb8c1a8492a807f503ebe6f7d0807e2
Daniel Ramirez ~ <danirg1414@outlook.com>
2022-05-22 14:18:55 -0500

    Objetos Js

--- js/index.js
+++ js/index.js
@@ -7,8 +7,30 @@ const password = document.getElementById('password');
 const repassword = document.getElementById('repassword');
 const regBtn = document.getElementById('regBtn');
 
+//Arrow function
+const registrarse = () => {
+    var userObj = {
+        nombre: nombre.value,
+        apellido: apellido.value,
+        usuario: usuario.value,
+        correo: correo.value,
+        password: password.value,
+    }
 
-regBtn.addEventListener('click',function(){
-    alert('hola mundo' + nombre.value);
+    var userObj2 = new User(
+        nombre.value,
+        apellido.value,
+        usuario.value,
+        correo.value,
+        password.value,
+    )
+    console.log(userObj);
+    console.log(userObj2);
 }
-);
+
+
+regBtn.addEventListener('click', registrarse);
+
+
+
+
